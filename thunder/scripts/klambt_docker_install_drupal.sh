8f6afe6c030ddd0c65c50499629af133a8bdf3fd
Weyand Tim <tim.weyand@R1717.local>
2016-08-12 13:14:30 +0200

    Logic for Thunder and all Drupal Mayor Versions
    @todo - cutomization

---
+++ thunder/scripts/klambt_docker_install_drupal.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$INSTALL_DRUPAL" = 1 ]; then
+    echo '#############################################'
+    echo '#            INSTALLING DRUPAL              #'
+    echo '#                                           #'
+    echo '#  This can be disabled during build with:  #'
+    echo '#  -e "INSTALL_DRUPAL=0"                    #'
+    echo '#                                           #'
+    echo '#############################################'
+
+    drush dl thunder --destination=/var/www/html
+    dir=`ls -l /var/www/html | awk '{print $9}' | grep 'drupal-'`
+    mv $dir/* .
+    chown -R www-data:www-data /var/www/html
+else
+    echo '#############################################'
+    echo '#          NOT INSTALLING DRUPAL            #'
+    echo '#                                           #'
+    echo '#  This can be enabled during build with:   #'
+    echo '#  -e "INSTALL_DRUPAL=1"                    #'
+    echo '#                                           #'
+    echo '#############################################'
+fi
+
+
