8aaafea9280146db5cd5fb5158485de06185b1fe
Weyand Tim <tim.weyand@R1717.local>
2016-08-12 13:36:48 +0200

    fix thunder build

--- thunder/scripts/klambt_docker_install_drupal.sh
+++ thunder/scripts/klambt_docker_install_drupal.sh
@@ -10,7 +10,7 @@ if [ "$INSTALL_DRUPAL" = 1 ]; then
     echo '#############################################'
 
     drush dl thunder --destination=/var/www/html
-    dir=`ls -l /var/www/html | awk '{print $9}' | grep 'drupal-'`
+    dir=`ls -l /var/www/html | awk '{print $9}' | grep 'thunder-'`
     mv $dir/* .
     chown -R www-data:www-data /var/www/html
 else
