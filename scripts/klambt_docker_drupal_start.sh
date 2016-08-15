dcb4b9911b2c625d26061d322340d47c9e503221
Weyand Tim <tim.weyand@R1717.local>
2016-08-15 10:49:46 +0200

    draft drupal install

--- scripts/klambt_docker_drupal_start.sh
+++ scripts/klambt_docker_drupal_start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+sleep 30
+echo '#############################################'
+echo '#          CLEAN INSTALL DRUPAL             #'
+echo '#                                           #'
+echo '#                                           #'
+echo '#############################################'
+drush site-install --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_LINK:$MYSQL_PORT/$MYSQL_DATABASE expert --locale=$DRUPAL_LOCALE --account-name=$DRUPAL_USERNAME --account-mail=$DRUPAL_USER_MAIL --account-pass=$DRUPAL_USER_PASSWORD --site-mail=$DRUPAL_SITE_MAIL --site-name=$DRUPAL_SITE_NAME
 
 
 apache2-foreground
--- scripts/klambt_docker_install_drupal.sh
+++ scripts/klambt_docker_install_drupal.sh
@@ -9,7 +9,7 @@ if [ "$INSTALL_DRUPAL" = 1 ]; then
     echo '#                                           #'
     echo '#############################################'
 
-    drush dl drupal --destination=/var/www/html
+    drush dl drupal-7 --destination=/var/www/html
     dir=`ls -l /var/www/html | awk '{print $9}' | grep 'drupal-'`
     mv $dir/* .
     chown -R www-data:www-data /var/www/html
