361dc8b23e70c6b3d063ed140207d8d02ad394e0
Weyand Tim <tim.weyand@R1717.local>
2016-08-15 14:00:24 +0200

    fix installation #2

--- scripts/klambt_docker_drupal_start.sh
+++ scripts/klambt_docker_drupal_start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sleep 30
+sleep 10
 echo '#############################################'
 echo '#          CLEAN INSTALL DRUPAL             #'
 echo '#                                           #'
 echo '#                                           #'
 echo '#############################################'
-drush site-install --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_LINK:$MYSQL_PORT/$MYSQL_DATABASE --locale=$DRUPAL_LOCALE --account-name=$DRUPAL_USERNAME --account-mail=$DRUPAL_USER_MAIL --account-pass=$DRUPAL_USER_PASSWORD --site-mail=$DRUPAL_SITE_MAIL
+cd $WORKDIR
+drush site-install --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_LINK:$MYSQL_PORT/$MYSQL_DATABASE --locale=$DRUPAL_LOCALE --account-name=$DRUPAL_USERNAME --account-mail=$DRUPAL_USER_MAIL --account-pass=$DRUPAL_USER_PASSWORD --site-mail=$DRUPAL_SITE_MAIL -y
 drush pm-enable -y $(grep -vE "^\s*#" /root/conf/drupal-7-modules.conf  | tr "\n" " ") 
 
 apache2-foreground
