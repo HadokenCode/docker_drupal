73ecffd2fa178a11a7917819ae79299a49860755
Tim Weyand <tim@weyand.biz>
2016-08-18 11:50:52 +0200

    Enable Drupal Modules one Module at a time

--- scripts/klambt_docker_drupal_start.sh
+++ scripts/klambt_docker_drupal_start.sh
@@ -16,23 +16,13 @@ cd $WORKDIR
 drush site-install --db-url=mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_LINK:$MYSQL_PORT/$MYSQL_DATABASE --locale=$DRUPAL_LOCALE --account-name=$DRUPAL_USERNAME --account-mail=$DRUPAL_USER_MAIL --account-pass=$DRUPAL_USER_PASSWORD --site-mail=$DRUPAL_SITE_MAIL -y
 
 drush pm-download -y $(grep -vE "^\s*#" /root/conf/drupal-7-modules.conf  | tr "\n" " ")
-drush pm-enable -y $(grep -vE "^\s*#" /root/conf/drupal-7-modules.conf  | tr "\n" " ")
 
-#$databases = array (
-#  'default' => 
-#  array (
-#    'default' => 
-#    array (
-#      'database' => 'drupal',
-#      'username' => 'drupal',
-#      'password' => 'drupal',
-#      'host' => 'drupal_database',
-#      'port' => '3306',
-#      'driver' => 'mysql',
-#      'prefix' => '',
-#    ),
-#  ),
-#);
+#enable one module at a time
+while read STRING
+do
+  STRING=${STRING%$'\r'}
+  drush pm-enable -y $STRING
+done < /root/conf/drupal-7-modules.conf
 
 
 echo '# Let the Server do the work';          >> sites/default/settings.php 
