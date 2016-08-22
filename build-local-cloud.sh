d1a35ce8cff69525c7ad42825781af0c7f575502
Weyand Tim <tim.weyand@R1717.local>
2016-08-22 15:36:12 +0200

    remove env and build-local-cloud from git

--- build-local-cloud.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#docker images | tail -n +2 | awk '{print $3}' > images.txt; docker rmi $(grep -vE "^\s*#" images.txt  | tr "\n" " ") -f; rm images.txt
-#docker ps -a | awk {'print $1'} | tail -n +2  > containers.txt; docker rm $(grep -vE "^\s*#" containers.txt  | tr "\n" " ") -f; rm containers.txt
-docker build -t klambt/drupal:latest .
-docker-compose up --force-recreate
